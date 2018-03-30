5073d5c997fc4ac3524be0787851b16d589f6838
yorkrj <31115331+yorkrj@users.noreply.github.com>
2018-03-29 23:17:29 -0400

    Created a couple simple Clipboard utils.

---
+++ ClipboardInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+
+namespace ClipboardInfo
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            IDataObject clipboardData = Clipboard.GetDataObject();
+
+            if (clipboardData == null)
+            {
+                Console.WriteLine("Clipboard is empty.");
+            }
+            else
+            {
+                Console.WriteLine("Clipboard contains the following formats:\n");
+
+                foreach (var format in clipboardData.GetFormats(false))
+                {
+                    Console.WriteLine("    {0}", format);
+                }
+            }
+
+#if DEBUG
+            Console.Write("\nPress any key to quit.");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
--- ScrubClipboardText/Program.cs
+++ ScrubClipboardText/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace ScrubClipboardText
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            if (Clipboard.ContainsText())
+            {
+                Clipboard.SetText(Clipboard.GetText(), TextDataFormat.Text);
+
+                Console.WriteLine("Text in clipboard has been scrubbed of formatting.");
+            }
+            else
+            {
+                Console.WriteLine("Clipboard does not contain text.");
+            }
+
+#if DEBUG
+            Console.Write("\nPress any key to quit.");
+            Console.ReadKey();
+#endif
         }
     }
 }
