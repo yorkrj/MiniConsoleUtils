1d8234a9aac042bb0bec3e711a69d72c0548f558
Rob York, Jr. <31115331+yorkrj@users.noreply.github.com>
2025-05-18 15:07:24 -0400

    Fixed issue with ClipboardScrubber where RTF and other content types needed to be handled distinctly in order to recognize text and remove formatting. All unit tests now pass.

--- ScrubClipboardText/ClipboardScrubber.cs
+++ ScrubClipboardText/ClipboardScrubber.cs
@@ -4,11 +4,29 @@ public class ClipboardScrubber
 {
     public static string ScrubClipboardText()
     {
+        bool success = false;
+        
         if (Clipboard.ContainsText())
         {
             Clipboard.SetText(Clipboard.GetText(), TextDataFormat.Text);
-            return "Text in clipboard has been scrubbed of formatting.";
+            success = true;
         }
-        return "Clipboard does not contain text.";
+        else if (Clipboard.ContainsText(TextDataFormat.Rtf))
+        {
+            Clipboard.SetText(Clipboard.GetText(TextDataFormat.Rtf), TextDataFormat.Text);
+            success = true;
+        }
+        else if (Clipboard.ContainsText(TextDataFormat.Html))
+        {
+            Clipboard.SetText(Clipboard.GetText(TextDataFormat.Html), TextDataFormat.Text);
+            success = true;
+        }
+        else if (Clipboard.ContainsText(TextDataFormat.UnicodeText))
+        {
+            Clipboard.SetText(Clipboard.GetText(TextDataFormat.UnicodeText), TextDataFormat.Text);
+            success = true;
+        }
+
+        return success ? "Text in clipboard has been scrubbed of formatting." : "Clipboard does not contain text.";
     }
 } 
